bd1fea657b5af70009eba63248d46ac011e1e8ec
Kevin Wu <kevin.wu@ucsb.edu>
2017-11-13 09:55:22 -0800

    chain git permissions together into one workflow

--- src/Components/Viewer.js
+++ src/Components/Viewer.js
@@ -2,48 +2,46 @@ import React from 'react'
 
 const Viewer = ({ gitPermissions, entityFilter }) => {
   const renderRepos = entityFilter => {
-    const filteredPerms = gitPermissions
+    const perms = gitPermissions
       .map(part => {
-        const { permissions } = part
-
-        const filteredPermissions = permissions.filter(
+        const filteredPermissions = part.permissions.filter(
           perm =>
             entityFilter
               ? perm.entityName && perm.entityName.includes(entityFilter)
               : true
         )
-
         return {
           ...part,
           filteredPermissions
         }
       })
       .filter(part => part.filteredPermissions.length > 0)
+      .map(part => {
+        const { repoPath, filteredPermissions } = part
 
-    const perms = filteredPerms.map(part => {
-      const { repoPath, filteredPermissions } = part
+        const entityPerms = filteredPermissions.map(
+          (part, permissionsIndex) => {
+            const { entityName, allow } = part
 
-      const entityPerms = filteredPermissions.map((part, permissionsIndex) => {
-        const { entityName, allow } = part
-
-        const entityAllowPerms = allow.map(part => part.name).join(', ')
-        return (
-          <p key={permissionsIndex}>
-            <span className="entityName">{entityName}</span>:{' '}
-            <span className="italics">{entityAllowPerms}</span>
-          </p>
+            const entityAllowPerms = allow.map(part => part.name).join(', ')
+            return (
+              <p key={permissionsIndex}>
+                <span className="entityName">{entityName}</span>:{' '}
+                <span className="italics">{entityAllowPerms}</span>
+              </p>
+            )
+          }
         )
-      })
 
-      return [
-        <div className="item" key={repoPath}>
-          {repoPath}
-        </div>,
-        <div className="item" key={entityPerms}>
-          {entityPerms}
-        </div>
-      ]
-    })
+        return [
+          <div className="item" key={repoPath}>
+            {repoPath}
+          </div>,
+          <div className="item" key={entityPerms}>
+            {entityPerms}
+          </div>
+        ]
+      })
 
     return perms
   }
